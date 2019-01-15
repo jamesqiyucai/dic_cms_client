b3b67c6f92c42f349e5d526b5e7be627c163b32c
James Cai <caiqiyu@live.cn>
2019-01-15 09:41:14 +0800

    somehow magically merged master zhao's code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import {CoreModule} from './core/core.module';
+import {IDServiceImplementation} from './core/id.service.implementation';
+import { ToolkitModule } from './toolkit/toolkit.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -11,7 +13,10 @@ import {CoreModule} from './core/core.module';
   imports: [
     BrowserModule,
     CoreModule,
+    ToolkitModule,
+    BrowserAnimationsModule
   ],
+  providers: [IDServiceImplementation],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
