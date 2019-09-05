b21ff2f15132f3b37b7844908a00205fd67609a8
jamestsai <caiqiyu@live.cn>
2019-09-05 23:12:29 +0800

    mess

---
+++ src/app/common_entities/translation.ts
@@ -0,0 +1,22 @@
+export abstract class Translation {
+  protected _id: number;
+  protected _text: string;
+  protected constructor(id: number, text: string) {
+    this._id = id;
+    this._text = text;
+  }
+  protected changeID(newID: number) {
+    if (this._id !== newID) {
+      this._id = newID;
+    } else {
+      console.log(`assigning the same id for translation ${this._id}`);
+    }
+  }
+  protected changeText(newText: string) {
+    if (this._text !== newText) {
+      this._text = newText;
+    } else {
+      console.log(`assigning a same text for translation ${this._id}`);
+    }
+  }
+}
