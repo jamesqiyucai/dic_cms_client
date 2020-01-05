2ff85540fa347d0bcbc59f3f6ea3d2bab5e9bf72
jihuzzi <vones3@outlook.com>
2020-01-05 22:12:47 +0800

    module files to be refactored

--- src/utils/fromJsGreedy.function.ts
+++
@@ -1,8 +0,0 @@
-import {Seq} from 'immutable';
-
-export function fromJSGreedy(js) {
-  return typeof js !== 'object' || js === null ? js :
-    Array.isArray(js) ?
-      Seq(js).map(fromJSGreedy).toList() :
-      Seq(js).map(fromJSGreedy).toMap();
-}
